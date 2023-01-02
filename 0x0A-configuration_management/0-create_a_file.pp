8d8b22bd5608f49621253a4fd5fd252925913b43
absow <abdullahibashirsow@gmail.com>
2023-01-02 20:43:13 +0100

    commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-#Create  file in /tmp
+#Create a file in /tmp
 
-file {`/tmp/school`:
-	ensure => file,
-	path => `/tmp/school`,
-	mode => `0744`,
-	owner => `ww-data`,
-	group => `ww-data',
-	content => `I love Puppet`
+file { '/tmp/school':
+    ensure  => file,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
